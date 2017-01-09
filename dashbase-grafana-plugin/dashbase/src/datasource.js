09c3f2099f25da15102afa95a3357b7f3850f278
Kevin Hou <kevin.hou@sjsu.edu>
2017-01-09 11:26:41 -0800

    moved request to helper method

--- dashbase-grafana-plugin/dashbase/src/datasource.js
+++ dashbase-grafana-plugin/dashbase/src/datasource.js
@@ -1,22 +1,42 @@
 export class DashbaseDatasource {
 
-	constructor(instanceSettings, $q, backendSrv, templateSrv) {
+	constructor(instanceSettings, $q, backendSrv, templateSrv, timeSrv) {
+		this.basicAuth = instanceSettings.basicAuth;
+		this.withCredentials = instanceSettings.withCredentials;
 		this.type = instanceSettings.type;
 		this.url = instanceSettings.url;
 		this.name = instanceSettings.name;
 		this.q = $q;
 		this.backendSrv = backendSrv;
 		this.templateSrv = templateSrv;
+		this.timeSrv = timeSrv;
+	}
+
+	query(options) {
+		console.log(options);
 	}
 
 	testDatasource() {
-		return this.backendSrv.datasourceRequest({
-			url: this.url + '/',
-			method: 'GET'
-		}).then(response => {
+		return this._request("GET", "").then(response => {
 			if (response.status === 200) {
 				return { status: "success", message: "Data source is working.", title: "Success" };
 			}
 		});
+	}
+
+	_request(method, endpoint) {
+		var options = {
+			url: this.url + "/" + endpoint,
+			method: method,
+		}
+		if (this.basicAuth || this.withCredentials) {
+        options.withCredentials = true;
+      }
+      if (this.basicAuth) {
+        options.headers = {
+          "Authorization": this.basicAuth
+        };
+      }
+		return this.backendSrv.datasourceRequest(options);
 	}
 }
