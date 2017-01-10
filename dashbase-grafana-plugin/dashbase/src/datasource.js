e94f97aebc6206cab56aa3e49081c7c72222b5e7
Kevin Hou <kevin.hou@sjsu.edu>
2017-01-09 23:29:05 -0800

    added query controller for query template and temp handling of metricFindQuery function

--- dashbase-grafana-plugin/dashbase/src/datasource.js
+++ dashbase-grafana-plugin/dashbase/src/datasource.js
@@ -13,7 +13,29 @@ export class DashbaseDatasource {
 	}
 
 	query(options) {
-		console.log(options);
+		// options contains the request object
+		var query = "";
+		var target;
+		var sentTargets = [];
+		// currently only supports a single query
+		for (var i = 0; i < options.targets.length; i++) {
+			target = options.targets[i];
+			if (target.hide) {
+				continue;
+			}
+			sentTargets.push(target);
+			query = target.target;
+		}
+		if (sentTargets.length === 0) {
+			return $q.when([]);
+		}
+		return this._post("sql", query);
+	}
+
+	metricFindQuery(options){
+		return this._request("GET", "get-info").then(function(results) {
+			// fields available from get=info endpoint
+		});
 	}
 
 	testDatasource() {
@@ -24,19 +46,26 @@ export class DashbaseDatasource {
 		});
 	}
 
-	_request(method, endpoint) {
+	_request(method, endpoint, data) {
 		var options = {
 			url: this.url + "/" + endpoint,
 			method: method,
+			data: data,
 		}
 		if (this.basicAuth || this.withCredentials) {
-        options.withCredentials = true;
-      }
-      if (this.basicAuth) {
-        options.headers = {
-          "Authorization": this.basicAuth
-        };
-      }
+			options.withCredentials = true;
+		}
+		if (this.basicAuth) {
+			options.headers = {
+				"Authorization": this.basicAuth
+			};
+		}
 		return this.backendSrv.datasourceRequest(options);
+	}
+
+	_post(endpoint, data) {
+		return this._request("POST", endpoint, data).then(function(results) {
+			console.log(results);
+		});
 	}
 }
--- dashbase-grafana-plugin/dashbase/src/query_ctrl.js
+++ dashbase-grafana-plugin/dashbase/src/query_ctrl.js
@@ -1,3 +1,27 @@
-export class DashbaseDatasourceQueryCtrl{
-	
+import {QueryCtrl} from 'app/plugins/sdk';
+
+export class DashbaseDatasourceQueryCtrl extends QueryCtrl {
+	constructor($scope, $injector, uiSegmentSrv) {
+		super($scope, $injector);
+
+		this.scope = $scope;
+		this.uiSegmentSrv = uiSegmentSrv;
+		this.target.target = this.target.target || 'select ts(day)';
+		this.target.type = this.target.type || 'sql';
+	}
+
+	getOptions() {
+		return this.datasource.metricFindQuery(this.target).then(this.uiSegmentSrv.transformToSegments(false));
+    // Options have to be transformed by uiSegmentSrv to be usable by metric-segment-model directive
+  }
+
+    toggleEditorMode() {
+    	this.target.rawQuery = !this.target.rawQuery; // show textarea vs input box
+    }
+
+    onChangeInternal() {
+    this.panelCtrl.refresh(); // request for panel refresh
+  }
 }
+
+DashbaseDatasourceQueryCtrl.templateUrl = 'partials/query.editor.html';
