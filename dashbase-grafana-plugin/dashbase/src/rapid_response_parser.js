8adede6407999ba9f6350e6389d8833bc9279328
Kevin Hou <kevin.hou@sjsu.edu>
2017-01-17 15:27:26 -0800

    working hits table

--- dashbase-grafana-plugin/dashbase/src/rapid_response_parser.js
+++ dashbase-grafana-plugin/dashbase/src/rapid_response_parser.js
@@ -14,23 +14,53 @@ export class RapidResponseParser {
 			console.log(this.response.data.error); // log out parse error
 			return this.response;
 		}
-		for (var i = 0; i < sentTargets.length; i++) {
-			if (sentTargets[i].type == "timeseries") {
-				target = this.response.data.aggregations[sentTargets[0].alias]; // change to i for when implementing support for multi queries per graph
-				if (!target) return this.response; // no response from bad query
-				if (target.histogramBuckets) {
-					var buckets = target.histogramBuckets;
-					dataArr.push({
-						"target": sentTargets[i].alias,
-						"datapoints": _.map(buckets, bucket => {
-							return [bucket.count, bucket.timeInSec * 1000]
-						}) 
-					});
-				}
-			} else {
-				target = this.response.data.hits;
+		console.log(sentTargets);
+		if (sentTargets[0].type == "timeseries") { // graph format
+			console.log("YOLO");
+			target = this.response.data.aggregations[sentTargets[0].alias]; // change to i for when implementing support for multi queries per graph
+			if (!target) return this.response; // no response from bad query
+			if (target.histogramBuckets) {
+				var buckets = target.histogramBuckets;
+				dataArr.push({
+					"target": sentTargets[0].alias,
+					"datapoints": _.map(buckets, bucket => {
+						return [bucket.count, bucket.timeInSec * 1000]
+					}) 
+				});
 			}
+		} else { // table format
+			var hits = this.response.data.hits;
+			console.log(hits);
+			dataArr = [{
+				"columns": [
+				{
+					"text": "DATE",
+					"type": "date",
+					"sort": true,
+					"desc": true,
+				},
+				{
+					"text": "RESPONSE"
+				},
+				{
+					"text": "HOST"
+				},
+				{
+					"text": "BYTESSENT",
+					"sort": true
+				},
+				{
+					"text": "RAW"
+				}
+				],
+				"rows": _.map(hits, hit => {
+					return [hit.timeInSec, hit.payload.fields.response[0], hit.payload.fields.host[0], hit.payload.fields.bytesSent, hit.payload.stored]
+				}),
+				"type": "table"
+			}
+			];
 		}
+		console.log(dataArr);
 		this.response.data = dataArr;
 		return this.response;
 	}
