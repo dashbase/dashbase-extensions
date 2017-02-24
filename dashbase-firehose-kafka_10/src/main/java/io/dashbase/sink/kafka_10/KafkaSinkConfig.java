bd6235ef7bd2c6e53858b8d3fbfcc581b6f86974
Kevin Hou <kevin@dashbase.io>
2017-02-23 23:26:55 -0800

    init kafka sink config

---
+++ dashbase-firehose-kafka_10/src/main/java/io/dashbase/sink/kafka_10/KafkaSinkConfig.java
@@ -0,0 +1,11 @@
+package io.dashbase.sink.kafka_10;
+
+import com.google.common.collect.Maps;
+import java.util.Map;
+
+public class KafkaSinkConfig {
+  public String hosts;
+  public String acks;
+  public int batchSize;
+  public Map<String, String> kafkaProps = Maps.newHashMap();
+}
