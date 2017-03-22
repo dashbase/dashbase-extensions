507c145a92cd87c6fbe37675fa4f6af5f8bf398a
Kevin Hou <kevin@dashbase.io>
2017-03-22 14:34:50 -0700

    added aws configure

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,8 +5,22 @@
 #########################
 VERSION=0.0.1
 
-# Upload Grafana tarball
-aws s3 cp dashbase-grafana/target/dashbase-grafana-datasource-$VERSION.tar.gz s3://dashbase-builds/master/dashbase-grafana-datasource-$VERSION.tar.gz
+configure_aws_cli() {
+  aws --version
+  aws configure set default.region us-west-1
+}
 
-# Update Grafana server
-ssh ec2-user@staging.dashbase.io "./update_grafana.sh"
+upload_to_s3() {
+  # Upload Grafana tarball
+  aws s3 cp dashbase-grafana/target/dashbase-grafana-datasource-$VERSION.tar.gz s3://dashbase-builds/master/dashbase-grafana-datasource-$VERSION.tar.gz
+
+  # Update Grafana server
+  ssh ec2-user@staging.dashbase.io "./update_grafana.sh"
+
+}
+
+# Configure AWS
+configure_aws_cli
+
+# Push latest to S3
+upload_to_s3
