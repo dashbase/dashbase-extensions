b8cf9d51c7d9f232689b3d651060ce5be8ac0d1b
Kevin Hou <kevin@dashbase.io>
2017-03-22 13:19:41 -0700

    WIP; revert structure change

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,7 +6,7 @@
 VERSION=0.0.1
 
 # Upload Grafana tarball
-aws s3 cp dashbase-grafana/target/dashbase-grafana-datasource-$VERSION.tar.gz s3://dashbase-builds/master/dashbase-grafana-datasource-$VERSION.tar.gz
+aws s3 cp dashbase-grafana/dashbase/target/dashbase-grafana-datasource-$VERSION.tar.gz s3://dashbase-builds/master/dashbase-grafana-datasource-$VERSION.tar.gz
 
 # Update Grafana server
 ssh ec2-user@staging.dashbase.io "./update_grafana.sh"
