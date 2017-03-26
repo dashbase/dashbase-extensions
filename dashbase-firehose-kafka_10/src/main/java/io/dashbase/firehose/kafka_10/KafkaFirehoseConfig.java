71a953cb4aa7a7bf9349293e99ea15eb1544e670
John Wang <john.wang@gmail.com>
2017-03-26 08:42:24 -0700

    clean up kafka (#38)

--- dashbase-firehose-kafka_10/src/main/java/io/dashbase/firehose/kafka_10/KafkaFirehoseConfig.java
+++ dashbase-firehose-kafka_10/src/main/java/io/dashbase/firehose/kafka_10/KafkaFirehoseConfig.java
@@ -1,6 +1,8 @@
 package io.dashbase.firehose.kafka_10;
 
+import java.util.Collections;
 import java.util.Map;
+import java.util.Set;
 
 import com.google.common.collect.Maps;
 
@@ -8,8 +10,7 @@ public class KafkaFirehoseConfig {
   public String hosts;
   public String groupId;
   public String topic;
-  public boolean enableAutoCommit = false;
-  public String autoOffsetReset = "latest";
   public int pollIntervalMs = Kafka10Firehose.DEFAULT_POLL_INTERVAL_MS;
+  public Set<Integer> partitions = Collections.emptySet();
   public Map<String, String> kafkaProps = Maps.newHashMap();
 }
