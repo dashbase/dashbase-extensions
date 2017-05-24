cea9837ca2da17d90f22eaeebed47a8843ff2eb1
John Wang <john.wang@gmail.com>
2017-05-24 15:20:13 -0700

    added config to bundle and test kafka firehose (#53)

---
+++ dashbase-firehose-kafka_10/src/main/java/io/dashbase/firehose/kafka_10/Kafka10FirehoseRunner.java
@@ -0,0 +1,9 @@
+package io.dashbase.firehose.kafka_10;
+
+import io.dashbase.app.firehose.FirehoseRunnerApplication;
+
+public class Kafka10FirehoseRunner {
+    public static void main(String[] args) throws Exception  {
+        new FirehoseRunnerApplication().run(args);
+    }
+}
