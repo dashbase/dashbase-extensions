ab107490bac597a108a6ba966c1354449adf74d5
John Wang <john@dashbase.io>
2017-05-31 13:40:00 -0700

    removed kafka firehose

--- dashbase-firehose-kafka_10/src/main/java/io/dashbase/firehose/kafka_10/Kafka10FirehoseRunner.java
+++
@@ -1,9 +0,0 @@
-package io.dashbase.firehose.kafka_10;
-
-import io.dashbase.app.firehose.FirehoseRunnerApplication;
-
-public class Kafka10FirehoseRunner {
-    public static void main(String[] args) throws Exception  {
-        new FirehoseRunnerApplication().run(args);
-    }
-}
--- dashbase-firehose-kafka_10/src/main/java/io/dashbase/firehose/kafka_10/KafkaOffset.java
+++
@@ -1,7 +0,0 @@
-package io.dashbase.firehose.kafka_10;
-
-import it.unimi.dsi.fastutil.ints.Int2LongOpenHashMap;
-
-public class KafkaOffset {
-  public Int2LongOpenHashMap offsetMap = new Int2LongOpenHashMap();
-}
